a2d3e1cea820257e50fbf03b5c4ddd27b4d9ba14
zj <12015237@hdu.edu.cn>
2018-01-31 22:21:23 +0800

    添加了ADCT自适应算法的实现基础框架

--- ADCT.c
+++ ADCT.c
@@ -3,6 +3,45 @@
 //
 
 #include "ADCT.h"
+#include "Cache.h"
+//定义自适应的读写缓冲区阈值调整函数,函数返回的是更新后的Tau值
+int ADCT_UpdateTau(int lastTau)
+{
+    int Tau,D_Tau,TempTau,MinTau,MaxTau;//表示脏队列的目标长度
+    //四射五入
+    double B_CLRU,B_DLRU;//表示各自队列的单位收益
+    MinTau=(int)(MinTauRatio*buf_size+0.5);
+    MaxTau=(int)(MaxTauRatio*buf_size+0.5);
+
+    D_Tau=buf_size-Tau;
+
+    //如果从底层得到周期的读写时延
+    ave_flash_read_delay=cycle_flash_read_delay/cycle_physical_read;
+    ave_flash_write_delay=cycle_flash_write_delay/cycle_physical_write;
+    B_CLRU=(CDHit_CRH*ave_flash_read_delay+CDHit_CWH*ave_flash_write_delay)/Tau;
+    B_DLRU=(CDHit_DRH*ave_flash_read_delay+CDHit_CWH*ave_flash_write_delay)/D_Tau;
+
+
+    //四舍五入
+    TempTau=(int)((B_CLRU/(B_CLRU+B_DLRU)*buf_size)+0.5);
+    TempTau=myMax(MinTau,TempTau);
+    TempTau=myMin(MaxTau,TempTau);
+    Tau=TempTau;
+
+
+    //重置相应的周期统计变量
+    CDHit_CWH=0;
+    CDHit_CRH=0;
+    CDHit_DRH=0;
+    CDHit_DWH=0;
+    T_count=1;
+    cycle_physical_write=0;
+    cycle_physical_read=0;
+    cycle_flash_write_delay=0.0;
+    cycle_flash_read_delay=0.0;
+
+    return Tau;
+}
 
 int ADCT_init(int size, int DataBlk_Num)
 {
--- ADCT.h
+++ ADCT.h
@@ -5,6 +5,15 @@
 #ifndef SIMULATION_ADCT_H
 #define SIMULATION_ADCT_H
 #include "type.h"
+#inlcude ""
 struct cache_operation * ADCT_op_setup();
+
+//设置对应的缓冲区大小
+int ADCT_MAX_CACHE_SIZE;
+int ADCT_CLRU_CACHE_SIZE;
+int ADCT_DLRU_CACHE_SIZE;
+
+//设置对应的链表头部节点
+
 
 #endif //SIMULATION_ADCT_H
